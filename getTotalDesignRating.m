17d86e5007e09634955af255b7ed64811e323a31
tstagge <tystagge@outlook.com>
2017-03-28 19:49:53 -0400

    UPDATED: optimization(), getTotalDesignRating()

--- getTotalDesignRating.m
+++ getTotalDesignRating.m
@@ -1,4 +1,5 @@
-function tdr = getTotalDesignRating(eOut, eIn, cTotal)
+function tdr = getTotalDesignRating(eIn, cTotal)
+    eOut = 12; %MWh (assumed a constant, desired output for all designs)
     nTotal = eOut/eIn;
     tdr = nTotal/cTotal;
 end
--- optimization.m
+++ optimization.m
@@ -1,13 +1,46 @@
 % Optimization, bitch!
 
-%% CATALOG DATA IO
+%% SITE DATA (Currently copied from main Proj2.m file)
+
+%Site 1
+1height = 30; %m
+1distRiver = 60; %m
+1minPipeLen = 67.08; %m
+1elevAngle = 30; %degrees
+1maxArea = 360,000; %m^2
+1maxVol = 7,200,000; %m^3
+1numBends = 0;
+1bendAngles = [0]; %degrees
+
+%Site 2
+2height = 100; %m
+2distRiver = 130; %m
+2minPipeLen = 253.21; %m
+2elevAngle = 60; %degrees
+2maxArea = 25,617.38; %m^2
+2maxVol = 512,347.54; %m^3
+2numBends = 1;
+2bendAngles = [60]; %degrees
+
+
+%Site 3
+3height = 65; %m
+3distRiver = 91.2; %m
+3minPipeLen = 114.56; %m
+3elevAngle = 45.46; %degrees
+3maxArea = 39,760.78; %m^2
+3maxVol = 795,215.64; %m^3
+3numBends = 1;
+3bendAngles = [115]; %degrees
+
+%% CATALOG DATA INPUT
 
 %PIPES
 pipesRaw = load('catalog_pipes.txt');
 pipesRawDim = size(pipesRaw);
-pipesNumQual = pipesRawDim(2)-1; %Number of qualities of pipes in catalog
+pipesNumFric = pipesRawDim(2)-1; %Number of qualities of pipes in catalog
 
-pipesDarcyFric = pipesRaw(1,1:pipesNumQual); %Row array of friction factors corresponding to quality
+pipesDarcyFric = pipesRaw(1,1:pipesNumFric); %Row array of friction factors corresponding to quality
 pipesDiameters = pipesRaw(2:end,1); %Column array of diameters
 pipesCosts = pipesRaw(2:end, 2:end); %Rows vary diameter; Columns vary quality
 
@@ -24,18 +57,28 @@ fittingsCost = fittingsRaw(3:end, 2:end); %Rows vary ID; Columns vary angle (and
 %PUMPS
 pumpsRaw = load('catalog_pumps.txt');
 pumpsRawDim = size(pumpsRaw);
-pumpsNumQual = pumpsRawDim(2)-1; %Number of qualities of pumps in catalog
+pumpsNumEfficiencies = pumpsRawDim(2)-1; %Number of qualities of pumps in catalog
 
-pumpsEfficiency = pumpsRaw(1,1:pumpsNumQual); %Row array of efficiencies corresponding to quality
+pumpsEfficiency = pumpsRaw(1,1:pumpsNumEfficiencies); %Row array of efficiencies corresponding to quality
 pumpsEPR = pumpsRaw(2:end,1); %Column array of EPRs
 pumpsCostPerQ = pumpsRaw(2:end, 2:end); %Rows vary EPR; Columns vary quality
 
 %TURBINES
 turbinesRaw = load('catalog_turbines.txt');
 turbinesRawDim = size(turbinesRaw);
-turbinesNumQual = turbinesRawDim(2)-1; %Number of qualities of turbines in catalog
+turbinesNumEfficiencies = turbinesRawDim(2)-1; %Number of qualities of turbines in catalog
 
-turbinesEfficiency = turbinesRaw(1,1:turbinesNumQual); %Row array of efficiencies corresponding to quality
+turbinesEfficiency = turbinesRaw(1,1:turbinesNumEfficiencies); %Row array of efficiencies corresponding to quality
 turbinesEPD = turbinesRaw(2:end,1); %Column array of EPDs
 turbinesCostPerQ = turbinesRaw(2:end, 2:end); %Rows vary EPD; Columns vary quality
+
+
+
+%% DESIGN ITERATION
+
+% REFERENCE: Function calls
+% Masefield(Eout, nT, f, L, D, xi, q, d, h)
+% EnergyInRequired(m, nP, f, L, D, xi, q, d, h)
+
+%Need to check if mass
 
